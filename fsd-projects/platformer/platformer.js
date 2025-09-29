545470050fff5c74734de20740b85b6c26ed7bcd
browniego30 <bailey.thomas@jpschools.org>
2025-09-29 14:05:05 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -26,21 +26,33 @@ $(function () {
     // ONLY CHANGE BELOW THIS POINT //
     //////////////////////////////////
 
-    // TODO 1 - Enable the Grid
-    // toggleGrid();
+     // TODO 1 - Enable the Grid
+    toggleGrid();
 
 
-    // TODO 2 - Create Platforms
-
-
+     // TODO 2 - Create Platforms
+createPlatform(300, 600, 40, 150);
+createPlatform(1350, 450, 50, 50, "red");
+createPlatform(500, 500, 40, 250);
+createPlatform(650, 600, 40, 140);
+createPlatform(800, 700, 40, 40);
+createPlatform(1050, 500, 40, 230);
 
 
     // TODO 3 - Create Collectables
+createCollectable("diamond", 200, 690);
+createCollectable("steve", 300, 560);
+createCollectable("max", 410, 400);
+createCollectable("grace", 800, 700);
+createCollectable("database", 1200, 400);
 
 
-
-    
     // TODO 4 - Create Cannons
+createCannon("top", 200, 2000);
+createCannon("right", 300, 2000);
+createCannon("top", 700, 2000);
+createCannon("top", 1000, 2000);
+
 
 
     
